514c4e6d3984ab1a8f7f2ffd713ae7db4f57ebef
0cRAY <xz452053@gmail.com>
2024-12-18 19:41:00 +0800

    1218

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2001/12/18/LCA /","2001/12/18/ans/"];function toRandomPost(){
+var posts=["2001/12/18/lca/","2001/12/18/ans/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
