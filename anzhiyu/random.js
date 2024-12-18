2451032dc17be47cc3ffc5795bdb9dda8e9995d8
0cRAY <xz452053@gmail.com>
2024-12-18 19:51:20 +0800

    1

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2001/12/18/lca/","2001/12/18/ans/"];function toRandomPost(){
+var posts=["2001/12/18/ans/","2001/12/18/lca/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
